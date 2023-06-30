0d241d62ce6032650e712fe5cd2a60efff6e4588
HongSeungGeun <103030115+HongSeungGeun@users.noreply.github.com>
2023-06-30 11:15:25 +0900

    Add files via upload

---
+++ script/app.js
@@ -0,0 +1,10 @@
+$('#open-btn').click(function(){
+    $('.show').css('display', 'block')
+    $('#open-btn').css('visibility', 'hidden')
+})
+
+$('#close-btn').click(function(){
+    $('.show').css('display', 'none')
+    $('#open-btn').css('visibility', 'visible')
+})
+
