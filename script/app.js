cfddd0ee92881c8e264c7c3e4057fc8cca7b35f2
HongSeungGeun <103030115+HongSeungGeun@users.noreply.github.com>
2023-06-30 12:52:35 +0900

    Add files via upload

--- script/app.js
+++ script/app.js
@@ -8,3 +8,36 @@ $('#close-btn').click(function(){
     $('#open-btn').css('visibility', 'visible')
 })
 
+
+
+$('.header-fixed').each(function(){
+    let win = $(window);
+    let main = $('.main-img');
+    let topspot = main.offset().top
+
+    console.log(topspot)
+
+    win.scroll(function(){
+        if(win.scrollTop() < topspot + 920 && win.scrollTop() > topspot - 1){
+            $('.header').css('color','white')    
+        }else {
+            $('.header').css('color', 'black')
+        }
+
+        if(win.scrollTop() > topspot + 920){
+            $('.header h1').css('visibility','hidden')
+        }else {
+            $('.header h1').css('visibility','visible')
+        }
+    })
+    
+})
+
+
+$(body).click(function(e){
+    if('.menu-bar'.has(e.target).length === 0) {
+        $('.menu-bar').heid();
+    }
+})
+
+
